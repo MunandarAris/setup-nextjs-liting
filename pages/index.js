a88dcfba0ced6ac746cdcf7f7f5c258fd78934d1
Aris Munandar <arismun0010@gmail.com>
2022-04-05 11:51:22 +0700

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function Home() {
+	function data() {}
 	return (
 		<div>
-			<h1>Selamat Datang !!!!!</h1>
+			<h1>Selamat Datang !!!!! {data()}</h1>
 			<p>
 				Mari kita belajar cara setup linting di aplikasi{' '}
 				<strong>Next Js</strong> kita untuk membuat pekerjaan kita semakin
